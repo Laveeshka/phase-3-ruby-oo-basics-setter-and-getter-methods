3e6b056274cb2fe3625011f484750ea34edf9af4
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-11-05 09:41:27 +1100

    commit tree 31a36bf2783bbd6ecb03004b9364328ba3b3d25b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    #setter method for @name instance variable
+    def name=(name)
+        @name = name
+    end
 
+    #getter method for @name instance variable
+    def name
+        @name
+    end
+
+    #setter method for @breed instance variable
+    def breed=(breed)
+        @breed = breed
+    end
+
+    #getter method for @breed instance variable
+    def breed
+        @breed
+    end
 end
